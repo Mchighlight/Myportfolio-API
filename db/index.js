3e8258142fa1219542390e6d916b3597e014a082
Henry Huang <Mchighlight@gmail.com>
2020-11-02 13:17:37 -0500

    create portfolio data

--- db/index.js
+++ db/index.js
@@ -1,17 +1,19 @@
-const config = require('../config/dev')
+
+const config = require('../config/dev');
 const mongoose = require('mongoose');
 
-exports.connect = () =>{
-    return mongoose.connect(config.DB_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-        useFindAndModify: false
-      }, (err) => {
-        if (err) { console.error(err); }
-        else {
-          console.log('Connected to DB!');
-        }
-      })
-}
+require('./models/portfolio');
 
+exports.connect = () => {
+  return mongoose.connect(config.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }, (err) => {
+    if (err) { console.error(err); }
+    else {
+      console.log('Connected to DB!');
+    }
+  })
+}
