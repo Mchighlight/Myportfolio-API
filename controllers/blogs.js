e203baa27ab7316b7e723aefb2537ba8c5c6f052
Henry Huang <Mchighlight@gmail.com>
2020-11-13 17:46:42 -0500

    delete blog

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,6 +2,7 @@
 
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const uniqueSlug = require('unique-slug');
 const Blog = mongoose.model('Blog');
 
 exports.getBlogs = async (req, res) => {
@@ -12,7 +13,10 @@ exports.getBlogs = async (req, res) => {
 
 exports.getBlogByUser = async (req, res) =>{
   const userId = req.user.sub;
-  const blogs = await Blog.find({userId}) ;
+  const blogs = await Blog.find({
+    userId,
+    status: { $in: ['draft', 'published']}
+  });
   return res.json(blogs);
 }
 
@@ -40,6 +44,20 @@ exports.createBlog = async (req, res) => {
 }
 
 
+const _saveBlog = async blog => {
+  try {
+    const createdBlog = await blog.save();
+    return createdBlog;
+  } catch(e) {
+    if (e.code === 11000 && e.keyPattern && e.keyPattern.slug) {
+      blog.slug += `-${uniqueSlug()}`;
+      return _saveBlog(blog);
+    }
+
+    throw(e);
+  }
+}
+
 exports.updateBlog = async (req, res) => {
   const { body, params: {id}} = req;
 
@@ -62,7 +80,7 @@ exports.updateBlog = async (req, res) => {
     blog.updateAt = new Date();
 
     try {
-      const updatedBlog = await blog.save();
+      const updatedBlog = await _saveBlog(blog);
       return res.json(updatedBlog);
     } catch(err) {
       return res.status(422).send(err.message);
